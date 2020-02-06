3fb8256032b1c3ace0e971e99ef34255d49daccf
Han <yinglinhan1987@gmail.com>
2020-02-06 20:25:59 +0800

    测试

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,31 +39,40 @@ const IndexPage = () => {
     // console.log(tempOffset)
     
   }
-  function dragFinish(e){
-    // console.log(e)
-    console.log(controller)
-    tempOffset.acumulateX += tempOffset.startX - e.clientX 
-    tempOffset.acumulateY += tempOffset.startY - e.clientY
-    tempOffset.x = parseInt(tempOffset.acumulateX / 640 ) !== 0 ? tempOffset.acumulateX % 640 : tempOffset.acumulateX
-    tempOffset.y = parseInt(tempOffset.acumulateY / 480 ) !== 0 ? tempOffset.acumulateY % 480 : tempOffset.acumulateY
-    // console.log(tempOffset)
-    //鼠标往上 左拖拽 数值是正数
-    //鼠标往下 右拖拽  负数
-    if(parseInt(tempOffset.acumulateX / 640 ) !== 0 || parseInt(tempOffset.acumulateY / 480 ) !== 0) {
-      console.log('需要重置',tempOffset)
-      // controller.set({
-      //   x:100,
-      //   y:100
-      // })
-      controller.set({
-        x:-(3840 - window.innerWidth/2) - tempOffset.x,
-        y:-(3840 - window.innerHeight/2) - tempOffset.y
+  // function dragFinish(e){
+  //   // console.log(e)
+  //   console.log(controller)
+  //   tempOffset.acumulateX += tempOffset.startX - e.clientX 
+  //   tempOffset.acumulateY += tempOffset.startY - e.clientY
+  //   tempOffset.x = parseInt(tempOffset.acumulateX / 640 ) !== 0 ? tempOffset.acumulateX % 640 : tempOffset.acumulateX
+  //   tempOffset.y = parseInt(tempOffset.acumulateY / 480 ) !== 0 ? tempOffset.acumulateY % 480 : tempOffset.acumulateY
+  //   // console.log(tempOffset)
+  //   //鼠标往上 左拖拽 数值是正数
+  //   //鼠标往下 右拖拽  负数
+  //   if(parseInt(tempOffset.acumulateX / 640 ) !== 0 || parseInt(tempOffset.acumulateY / 480 ) !== 0) {
+  //     console.log('需要重置',tempOffset)
+  //     // controller.set({
+  //     //   x:100,
+  //     //   y:100
+  //     // })
+  //     controller.set({
+  //       x:-(3840 - window.innerWidth/2) - tempOffset.x,
+  //       y:-(3840 - window.innerHeight/2) - tempOffset.y
         
-      })
-      tempOffset.acumulateX = tempOffset.x
-      tempOffset.acumulateY = tempOffset.y
+  //     })
+  //     tempOffset.acumulateX = tempOffset.x
+  //     tempOffset.acumulateY = tempOffset.y
 
-    }
+  //   }
+  // }
+  function dragStart(){
+    console.log(123)
+  }
+  function dragEnd(){
+    console.log(321)
+  }
+  function dragging(){
+    console.log(666)
   }
 
   return (
@@ -77,9 +86,12 @@ const IndexPage = () => {
 
     `}/>
     <motion.div 
-    drag
-    onDragStart={dragBegin}
-    onDragEnd={dragFinish}
+    // drag
+    // onDragStart={dragBegin}
+    // onDragEnd={dragFinish}
+    onTapStart={dragStart}
+    onPanEnd={dragEnd}
+    onPan={dragging}
     dragMomentum={false}
     animate={controller}
     initial={{x:-(3840 - window.innerWidth/2),y:-(3840 - window.innerHeight/2)}}
